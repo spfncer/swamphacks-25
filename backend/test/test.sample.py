03c0b2255e7188e1dff77c071e4cda8e748ffe00
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-01-25 10:36:35 -0500

    Add basic unit tests

---
+++ backend/test/test.sample.py
@@ -0,0 +1,10 @@
+"""
+TODO: Replace this file with acutal unit tests. (remember to update the pytest command in the build.backend.yml)
+"""
+
+
+def test_sample():
+    """
+    To be replaced with actual unit tests...
+    """
+    assert 5 == 5
