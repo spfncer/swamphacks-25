f763bcb54c3ccc1d5a4d501997bf763932928cc3
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-01-25 11:37:21 -0500

    Create Backend Data Models

--- backend/src/models/comment.py
+++ backend/src/models/comment.py
@@ -1,15 +1,20 @@
 from datetime import datetime
 from typing import Optional, List
+from typing_extensions import Annotated
 
 from pydantic import ConfigDict, BaseModel, Field
+from pydantic.functional_validators import BeforeValidator
 
 from bson import ObjectId
 
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 class CommentModel(BaseModel):
     """
     Represents a comment for our extension on the webpage.
     """
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+
     webpage: str = Field(...)
     author: str = Field(...)
     body: str = Field(...)
@@ -39,3 +44,22 @@ class CommentCollection(BaseModel):
     """
     comments: List[CommentModel]
 
+class UpdateCommentModel(BaseModel):
+    """
+    A set of optional updates to be made to a comment in the database.
+    """
+    webpage: Optional[str] = None
+    author: Optional[str] = None
+    body: Optional[str] = None
+
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        json_encoders={ObjectId: str},
+        json_schema_extra={
+            "example": {
+                "webpage": "www.example.com",
+                "author": "jdoe@example.com",
+                "body": "This webpage seems relatively accurate based on my experience."
+            }
+        },
+    )
