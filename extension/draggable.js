48eeee2ceaecfe041edebdd3fb6ee02a4fb807f3
Spencer Fasulo <spencer@fasulo.one>
2025-01-25 12:52:37 -0500

    Create Draggable script

---
+++ extension/draggable.js
@@ -0,0 +1,37 @@
+function makeElementDraggable(drag) {
+    const boundingBox = drag.getBoundingClientRect();
+
+    drag.style.x = `${boundingBox.x}px`;
+    drag.style.y = `${boundingBox.y}px`;
+    drag.style.position = "absolute";
+
+    drag.addEventListener("mousedown", (e) => {
+        const x = e.clientX - drag.getBoundingClientRect().left;
+        const y = e.clientY - drag.getBoundingClientRect().top;
+
+        drag.style.x = `${drag.getBoundingClientRect().left}px`;
+        drag.style.y = `${drag.getBoundingClientRect().top}px`;
+
+        const onMouseMove = (e) => {
+            drag.style.left = `${e.clientX - x}px`;
+            drag.style.top = `${e.clientY - y}px`;
+        }
+
+        const onMouseUp = () => {
+            window.removeEventListener("mousemove", onMouseMove);
+            window.removeEventListener("mouseup", onMouseUp);
+        }
+
+        window.addEventListener("mousemove", onMouseMove);
+        window.addEventListener("mouseup", onMouseUp);
+    });
+}
+window.makeElementDraggable = makeElementDraggable;
+console.log("Draggable script loaded", makeElementDraggable);
+
+window.addEventListener("DOMContentLoaded", () => {
+    const drags = document.querySelectorAll("[data-draggable]");
+    drags.forEach((drag) => {
+        makeElementDraggable(drag);
+    })
+})
