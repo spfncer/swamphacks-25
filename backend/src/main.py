d664a5bd6a7e673a5d8a52346895a8c3ef1ab85b
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-01-25 15:03:50 -0500

    Setup fastapi lifespan correctly

--- backend/src/main.py
+++ backend/src/main.py
@@ -24,24 +24,37 @@ tags_metadata = [
     }
 ]
 
+async def db_lifespan(app: FastAPI):
+    # Startup
+    app.mongodb_client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGODB_URL"))
+    app.database = app.mongodb_client.get_database("Comments")
+    app.comment_collection = app.database.get_collection("websites")
+
+    # Try pinging the database to ensure connection
+    ping_response = await app.database.command("ping")
+    if int(ping_response["ok"]) != 1:
+        raise Exception("Problem connecting to database cluster.")
+
+    yield
+
+    # Shutdown
+    app.mongodb_client.close()
+
 app = FastAPI(
     title="WebsiteCommentDatabase",
     description="A simple API to manage comments on a webpage through a chrome extension",
     version="0.1.0",
-    openapi_tags=tags_metadata
+    openapi_tags=tags_metadata,
+    lifespan=db_lifespan
 )
 
-# Setup Mongo Database Connection
-mongo_url = os.getenv("MONGODB_URL")
-client = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
-db = client.get_database("Comments")
-comment_collection = db.get_collection("websites")
+# Other
 
 @app.get("/", tags=["Other"])
 async def read_root():
     return {"Hello": "World"}
 
-# Upload comment
+# Comments
 
 @app.post(
     "/comments/",
@@ -56,14 +69,13 @@ async def create_comment(comment: CommentModel = Body(...)):
     Insert a new comment record.
     A unique `id` will be created and provided in the response.
     """
-    new_comment = await comment_collection.insert_one(
+    new_comment = await app.comment_collection.insert_one(
         comment.model_dump(by_alias=True, exclude=["id"])
     )
-    created_comment = await comment_collection.find_one(
+    created_comment = await app.comment_collection.find_one(
         {"_id": new_comment.inserted_id}
     )
     return created_comment
-
 
 @app.get(
     "/comments/",
@@ -77,7 +89,7 @@ async def list_comments():
     List all of the comment data in the database.
     The response is unpaginated and limited to 1000 results.
     """
-    return CommentCollection(comments=await comment_collection.find().to_list(1000))
+    return CommentCollection(comments=await app.comment_collection.find().to_list(1000))
 
 @app.get(
     "/comments/{id}",
@@ -91,7 +103,7 @@ async def get_comment(id: str):
     Get a single comment from the database by ID.
     """
     if (
-        comment := await comment_collection.find_one({"_id": ObjectId(id)})
+        comment := await app.comment_collection.find_one({"_id": ObjectId(id)})
     ) is not None:
         return comment
     raise HTTPException(status_code=404, detail=f"Comment {id} not found!")
@@ -114,7 +126,7 @@ async def update_comment(id: str, comment: UpdateCommentModel = Body(...)):
     }
 
     if len(comment) >= 1:
-        update_result = await comment_collection.find_one_and_update(
+        update_result = await app.comment_collection.find_one_and_update(
             {"_id": ObjectId(id)},
             {"$set": comment},
             return_document=ReturnDocument.AFTER,
@@ -125,12 +137,11 @@ async def update_comment(id: str, comment: UpdateCommentModel = Body(...)):
             raise HTTPException(status_code=404, detail=f"Comment {id} not found!")
 
     # The update is empty, but we should still return the matching document:
-    if (existing_comment := await comment_collection.find_one({"_id": id})) is not None:
+    if (existing_comment := await app.comment_collection.find_one({"_id": id})) is not None:
         return existing_comment
     
     # Something went wrong
     raise HTTPException(status_code=404, detail=f"Comment {id} not found!")
-
 
 @app.delete(
     "/comments/{id}", 
@@ -141,9 +152,8 @@ async def delete_comment(id: str):
     """
     Remove a single comment record from the database.
     """
-    delete_result = await comment_collection.delete_one({"_id": ObjectId(id)})
+    delete_result = await app.comment_collection.delete_one({"_id": ObjectId(id)})
     if delete_result.deleted_count == 1:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
 
     raise HTTPException(status_code=404, detail=f"Comment {id} not found!")
-
