875e391adf70fac7489d9d7dea996f2e500e0129
Spencer Fasulo <spencer@fasulo.one>
2025-01-26 11:06:46 -0500

    Frontend

--- search-frontend/tary/src/routes/+page.svelte
+++ search-frontend/tary/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="home-wrap flex h-screen flex-col items-center justify-center">
+	<h1 class="font-cormorant mb-4 text-8xl font-bold">Tary</h1>
+	<p class="font-oh-baby text-2xl">The web is yours to explore</p>
+	<div class="relative w-1/2">
+		<input
+			type="text"
+			placeholder="Search"
+			class="bg-slate white-focus-ring rounded-token w-full rounded-lg border border-gray-300 p-2"
+		/>
+		<button class="rounded-token p-2 text-white absolute right-2 top-0 bottom-0" aria-label="Search">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+				/>
+			</svg>
+		</button>
+	</div>
+</div>
--- search-frontend/tary/tailwind.config.ts
+++ search-frontend/tary/tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {colors: {'slate': '#303030'}}
 	},
 
 	plugins: [typography]
